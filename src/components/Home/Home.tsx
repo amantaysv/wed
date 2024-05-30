7818dc43e247fd67c1a7cf716e6041bbd7a0cde5
Amantay <amantay.sv@gmail.com>
2024-05-31 00:49:09 +0600

    capibara

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useRef } from 'react'
 import { RiArrowDownWideFill } from 'react-icons/ri'
+import sound from '../../assets/capibara.mp3'
 import s from './Home.module.scss'
 
 export const Home = () => {
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const onplay = () => audioRef?.current?.play()
+
+  useEffect(() => {
+    const audioElement = audioRef.current
+
+    if (!audioElement) return
+
+    audioElement.src = sound
+    audioElement.muted = false
+
+    audioElement.play()
+  }, [])
+
   return (
     <div className={s.home}>
+      <audio ref={audioRef} loop>
+        123
+      </audio>
       <div className={s.content}>
         <h1>Амантай - Мээрим</h1>
         <p>24/08/2024</p>
-        <button>
+        <button onClick={onplay}>
           <RiArrowDownWideFill />
         </button>
       </div>
